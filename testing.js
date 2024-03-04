622caddb5ce8c3daa2b7a2d14931411d646c7fba
Thong Pham <tpham73@my.bcit.ca>
2024-03-04 11:21:04 -0800

    init

---
+++ testing.js
@@ -0,0 +1,19 @@
+// console.log('hello world');
+// let name = 'john doe';
+
+// console.log(name);
+// console.log('john doe')
+// console.log(12)
+// console.log(12 / 2)
+// console.log(name.toUpperCase());
+
+let name; // variable declaration
+name = 'hello world';
+
+name = 5;
+console.log(name);
+
+const x = 10; //
+
+var y = 20; //discourage using this. its behaviour is not stable??
+
